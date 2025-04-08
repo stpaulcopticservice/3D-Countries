0f371d78a349b0be326baed6a4aad52d0bb72b77
stpaulcopticservice <copticservicestpaul@gmail.com>
2025-04-08 11:43:04 +0200

    Update script.js

--- script.js
+++ script.js
@@ -130,7 +130,7 @@ function rotateAndZoomToCountry(lat, lon) {
 const latRad = THREE.MathUtils.degToRad(lat);
 const lonRad = THREE.MathUtils.degToRad(lon);
 // Rotations to bring the point to front
-const targetRotationY = (THREE.MathUtils.degToRad(lon)*-1)+225;                // Longitude
+const targetRotationY = (THREE.MathUtils.degToRad(lon)*-1)+200;                // Longitude
 const targetRotationX = THREE.MathUtils.degToRad(lat); // Latitude
     
     // Target rotation in Three.js coordinate system

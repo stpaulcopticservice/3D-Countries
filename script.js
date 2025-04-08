7e9b08e5615c26d341209b1256554a6c752fcf5e
stpaulcopticservice <copticservicestpaul@gmail.com>
2025-04-08 10:39:46 +0200

    Update script.js

--- script.js
+++ script.js
@@ -131,7 +131,7 @@ const latRad = THREE.MathUtils.degToRad(lat);
 const lonRad = THREE.MathUtils.degToRad(lon);
 // Rotations to bring the point to front
 const targetRotationY = -lonRad;                // Longitude
-const targetRotationX = THREE.MathUtils.degToRad(lat - 90); // Latitude
+const targetRotationX = THREE.MathUtils.degToRad(lat); // Latitude
     
     // Target rotation in Three.js coordinate system
     // Earth’s Y-axis is horizontal (longitude), X-axis is vertical (latitude)
